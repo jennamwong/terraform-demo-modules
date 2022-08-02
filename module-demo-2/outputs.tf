5e908144c8498f57ddcd0e88d7877c35ed0bd73b
Jenna Wong <jenna.wong@hashicorp.com>
2022-08-02 15:34:40 -0500

    changing module name

--- module-demo-2/outputs.tf
+++ module-demo-2/outputs.tf
@@ -10,15 +10,15 @@ output "ec2_instance_public_ips" {
 
 output "website_bucket_arn" {
   description = "ARN of the bucket"
-  value       = module.website_s3_bucket.arn
+  value       = module.website-s3-bucket.arn
 }
 
 output "website_bucket_name" {
   description = "Name (id) of the bucket"
-  value       = module.website_s3_bucket.name
+  value       = module.website-s3-bucket.name
 }
 
 output "website_bucket_domain" {
   description = "Domain name of the bucket"
-  value       = module.website_s3_bucket.domain
+  value       = module.website-s3-bucket.domain
 }
